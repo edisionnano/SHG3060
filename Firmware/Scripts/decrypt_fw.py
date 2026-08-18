cfe46ca6c14dd7d22a572b77df19a1c64af642d9
edisionnano <samantas5855@gmail.com>
2026-08-18 17:39:25 +0300

    Get size in a more proper way

--- Firmware/Scripts/decrypt_fw.py
+++ Firmware/Scripts/decrypt_fw.py
@@ -1,19 +1,10 @@
 import hashlib
+import struct
 import sys
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 MAGIC = b"V0daf0n5_123@hybR1D@ccesshyf1234"
-
-
-def atol(field):
-    s = field.split(b"\x00", 1)[0].lstrip()
-    i = 1 if s[:1] in (b"+", b"-") else 0
-    j = i
-    while j < len(s) and s[j:j + 1].isdigit():
-        j += 1
-    return int(s[:j] or b"0")
-
 
 def decrypt_image(blob):
     f0, f1, f2, f3, f4 = (blob[i:i + 32] for i in range(0, 160, 32))
@@ -21,7 +12,7 @@ def decrypt_image(blob):
         hashlib.md5(f3 + f1).digest() + hashlib.md5(f4 + f1).digest(), MAGIC))
     dec = Cipher(algorithms.AES(key), modes.CBC(f2[:16])).decryptor()
     pt = dec.update(blob[160:]) + dec.finalize()
-    length = atol(f4)
+    length = struct.unpack(">I", f4[:4])[0]
     return pt[:length] if 0 <= length <= len(pt) else pt
 
 
